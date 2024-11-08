039e0d6833bf15ba76fa13b956aa81acf2770874
Wade Mealing <wmealing@gmail.com>
2024-11-08 18:43:04 +1000

    Initial project creation.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wmealing/resource-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wmealing/resource_test_test.clj
@@ -0,0 +1,7 @@
+(ns wmealing.resource-test-test
+  (:require [clojure.test :refer :all]
+            [wmealing.resource-test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
